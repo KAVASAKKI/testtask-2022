d4b789b278c10af893eaa916bf554819f38e5b93
Pavel Omelchenko <p.omel4enko2002@gmail.com>
2022-11-28 09:17:34 +0200

    Add "Tooltip" element

--- src/elements/Card/Card.tsx
+++ src/elements/Card/Card.tsx
@@ -1,14 +1,13 @@
-import ReactTooltip from "react-tooltip";
-import { Image } from "elements/Image/Image";
-// import { Tooltip } from "elements/Tooltip/Tooltip";
-import styles from "./Card.module.scss";
+import { Image } from 'elements/Image/Image';
+import { Tooltip, ReactTooltipCustom } from 'elements/Tooltip/Tooltip';
+import styles from './Card.module.scss';
 
 const issue = {
-  imgURL: "",
-  name: "Pavel",
-  position: "Senior frontend developer ffffffffffffffffffffffffffffffffffffff",
-  email: "p.omel4enko2002@gmail.com",
-  phone: "+380 93 105 50 84",
+  imgURL: '',
+  name: 'Pavel',
+  position: 'Senior frontend developer ffffffffffffffffffffffffffffffffffffff',
+  email: 'p.omel4enko2002@gmail.com',
+  phone: '+380 93 105 50 84',
 };
 
 export const Card = () => {
@@ -16,7 +15,7 @@ export const Card = () => {
     <div className={styles.container}>
       <Image />
 
-      {/* <Tooltip title={issue.name}>
+      <Tooltip title={issue.name}>
         <p className={styles.name}>{issue.name}</p>
       </Tooltip>
 
@@ -30,35 +29,9 @@ export const Card = () => {
 
       <Tooltip title={issue.phone}>
         <p className={styles.phone}>{issue.phone}</p>
-      </Tooltip> */}
+      </Tooltip>
 
-      <p data-tip={issue.name} className={styles.name}>
-        {issue.name}
-      </p>
-
-      <p data-tip={issue.position} className={styles.position}>
-        {issue.position}
-      </p>
-
-      <p data-tip={issue.email} className={styles.email}>
-        {issue.email}
-      </p>
-
-      <p data-tip={issue.phone} className={styles.phone}>
-        {issue.phone}
-      </p>
-
-      <ReactTooltip
-        place='bottom'
-        padding='3px 16px'
-        arrowColor='transparent'
-        delayShow={300}
-        delayHide={100}
-        overridePosition={({ left, top }: any, currentEvent: any) => ({
-          left: currentEvent.x,
-          top: currentEvent.y,
-        })}
-      />
+      <ReactTooltipCustom />
     </div>
   );
 };
--- src/elements/Tooltip/Tooltip.tsx
+++ src/elements/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import ReactTooltip from "react-tooltip";
+import ReactTooltip from 'react-tooltip';
 
 type TooltipType = { children: React.ReactNode; title: string };
 
@@ -6,18 +6,20 @@ export const Tooltip = ({ children, title }: TooltipType) => {
   return (
     <>
       <div data-tip={title}>{children}</div>
-
-      <ReactTooltip
-        place='bottom'
-        padding='3px 16px'
-        arrowColor='transparent'
-        delayShow={300}
-        delayHide={100}
-        overridePosition={({ left, top }: any, currentEvent: any) => ({
-          left: currentEvent.pageX - 10,
-          top: currentEvent.pageY + 20,
-        })}
-      />
     </>
   );
 };
+
+export const ReactTooltipCustom = () => (
+  <ReactTooltip
+    place="right"
+    padding="3px 16px"
+    arrowColor="transparent"
+    delayShow={300}
+    delayHide={100}
+    overridePosition={({ left, top }: any, currentEvent: any) => ({
+      left: currentEvent.x - 30,
+      top: currentEvent.y + 30,
+    })}
+  />
+);
